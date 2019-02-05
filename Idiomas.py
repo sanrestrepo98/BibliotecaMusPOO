131ed7b9c7776c0dfe851ccf42393d2153ab4bc1
Manuelfer96 <47194539+Manuelfer96@users.noreply.github.com>
2019-02-05 00:08:53 -0500

    idiiomas

--- Idiomas.py
+++ Idiomas.py
@@ -1,11 +1,32 @@
-import sys
-import main
 class Idioma:
-
-	def __init__(self):
-		pass
-
-	def IdiomaIngles (self,numero):
-		pass
-
-#No sé qué hacer aquí :C aiuda!
+	diccionarioMensajes = {}
+	#Acá van todos los mensajes en español, tenemos que definir cuáles serán, por el momento pondré el menú inicial
+	español = {
+	"opcioness": """
+		Opciones
+		1. Ver Nombre
+		2. Registrarse
+		3. Cambiar Nombre
+		4. Crear Playlist
+		5. Salir
+		""",
+	"opcion": "¿Qué desea hacer?: ",
+	"opcionNoValida": "{0} no es una opción válida",
+	"salir": "Hasta luego, vuelve pronto :c"
+	#Acá se van agregando todos los textos que querramos
+	}
+	
+	ingles = {
+	"operaciones": """
+		Options
+		1. Read Name
+		2. Sign up
+		3. Rename
+		4. Create Playlist
+		5. Exit
+		""",
+	"opcion": "What would you like to do?: ",
+	"opcionNoValida": "{0} isn't a valid option",
+	"salir": "Bye, see you later"
+	#El mismo comentario de arriba pero en inglés :v
+	}
--- Main.py
+++ Main.py
@@ -1,33 +1,40 @@
-import Usuario
-import PlayList
 import sys
+from Usuario import Usuario
+import PlayList
+from Idiomas import Idioma
 
 #Esta clase es para ejecutar el programa
 
 class Main:
 
-
 	def __init__(self):
 		self.break_while=1
-		self.choises ={
-		'1': self.VerNombre,
-        '2': self.RegistrarUs,
-        '3': self.CambiarNombre,
-        '4': self.CrearPlaylist,
-        '5': self.salir,
+		self.choices ={
+			"1": self.VerNombre,
+        	"2": self.RegistrarUs,
+       	 	"3": self.CambiarNombre,
+       		"4": self.CrearPlaylist,
+ 			"5": self.salir
         }
+	
 
 #Este método es para desplegar el menu
+	@staticmethod
+	def display_menu():
+		print(Idiomas.diccionarioMensajes.get("operaciones"))
 
-	def display_menu(self):
-		print("""
-        Operaciones
-        1. Ver nombre
-        2. Registrar usuario
-        3. Cambia nombre
-        4. Crear Playlist
-        5. Salir
-        """)
+#Este método es para elegir el idioma
+	@staticmethod
+	def idiomaMensajes():
+		print(""" Idioma:
+			1. Español
+			2. Ingles
+			""")
+		idioma = input("Seleccione un idioma: ")
+		if idioma =="1":
+			Idiomas.diccionarioMensajes = Idiomas.español
+		elif idioma =="2":
+			Idiomas.diccionarioMensajes = Idiomas.ingles
 
 #Este método es para ver el nombre (creo que es obvio)
 
@@ -37,16 +44,20 @@ class Main:
 #Este método es para ingresar usuarios
 
 	def RegistrarUs (self):
-		u1=Usuario()
+		
 		nombre=input("Ingrese nombre: ")
-		u1.setNombre(nombre)
+		
 		num=int(input("Ingrese id: "))
-		u1.setId(num)
+		
 		ema=input("Ingrese Email: ")
-		u1.setEmail(ema)
+		
 		pas=input("Ingrese contraseña: ")
-		u1.setPassword(pas)
-		Usuario.lista_us.append(u1)
+		
+		u1=Usuario(nombre,num,ema,pas)
+
+		Usuario().lista_us.append(u1)
+		
+		print(u1.getNombre())
 
 #Cambiar el nombre del usuario
 
@@ -56,34 +67,30 @@ class Main:
 #Crea Playlist
 
 	def CrearPlaylist(self):
-		p1=Playlist()
 		nom=input("Ingrese nombre de la Playlist")
-		p1.setNombreP(nom)
 		des=input("Ingrese descripcion de la Playlist")
-		p1.setDescripcion(des)
+		p1=Playlist(nom,des)
 		Playlist().lista_play.append(p1)
 
-
-	def salir (self):
-		print("Hasta luego")
-		sys.exit(0)
+#Este método es para salir de la aplicación(? (No estoy muy seguro :c)
+	@staticmethod
+	def salir():
+		print(Idiomas.listaMensajes.get("salir"))
+		sys._exit(0)
  
 
 #Aqui se corre el programa
      
 	def run (self):
+		Main.idiomaMensajes()
 		while self.break_while==1:
 			self.display_menu()
-			opcion = input("Ingrese una opcion: ")
+			opcion = input(Idiomas.diccionarioMensajes.get("opcion"))
 			action = self.choices.get(opcion)
 			if action:
 				action()
 			else:
-				print("{0} no es una opcion valida".format(opcion))
+				print(Idiomas.diccionarioMensajes.get("opcionNoValida").format(opcion))
                
 if __name__ == "__main__":
-   m=Main()
-   m.run()
-
-
-
+   Main().run()
