b5f0ce02f3f87342ab55ba0dede9a75718358b7e
Ext4ur <47161530+Ext4ur@users.noreply.github.com>
2019-02-04 19:22:16 -0500

    Se configuró el menú en base al idioma
    1. Se cambió display_menu
    2. Se creó un método para elegir idioma
    3. Se cambiaron los textos en base a la clase Idioma

--- Main.py
+++ Main.py
@@ -1,33 +1,39 @@
+import sys
 import Usuario
 import PlayList
-import sys
+from Idiomas import Idioma
 
 #Esta clase es para ejecutar el programa
 
 class Main:
 
-
 	def __init__(self):
 		self.break_while=1
-		self.choises ={
-		'1': self.VerNombre,
-        '2': self.RegistrarUs,
-        '3': self.CambiarNombre,
-        '4': self.CrearPlaylist,
-        '5': self.salir,
+		self.choices ={
+			"1": self.VerNombre,
+        	"2": self.RegistrarUs,
+       	 	"3": self.CambiarNombre,
+       		"4": self.CrearPlaylist,
+ 			"5": self.salir
         }
 
 #Este método es para desplegar el menu
+	@staticmethod
+	def display_menu():
+		print(Idioma.diccionarioMensajes.get("operaciones"))
 
-	def display_menu(self):
-		print("""
-        Operaciones
-        1. Ver nombre
-        2. Registrar usuario
-        3. Cambia nombre
-        4. Crear Playlist
-        5. Salir
-        """)
+#Este método es para elegir el idioma
+	@staticmethod
+	def idiomaMensajes():
+		print(""" Idioma:
+			1. Español
+			2. Ingles
+			""")
+		idioma = input("Seleccione un idioma: ")
+		if idioma =="1":
+			Idioma.diccionarioMensajes = Idioma.español
+		elif idioma =="2":
+			Idioma.diccionarioMensajes = Idioma.ingles
 
 #Este método es para ver el nombre (creo que es obvio)
 
@@ -63,10 +69,11 @@ class Main:
 		p1.setDescripcion(des)
 		Playlist().lista_play.append(p1)
 
-
-	def salir (self):
-		print("Hasta luego")
-		sys.exit(0)
+#Este método es para salir de la aplicación(? (No estoy muy seguro :c)
+	@staticmethod
+	def salir():
+		print(Idioma.listaMensajes.get("salir"))
+		sys._exit(0)
  
 
 #Aqui se corre el programa
@@ -74,16 +81,15 @@ class Main:
 	def run (self):
 		while self.break_while==1:
 			self.display_menu()
-			opcion = input("Ingrese una opcion: ")
+			opcion = input(Idioma.diccionarioMensajes.get("opcion"))
 			action = self.choices.get(opcion)
 			if action:
 				action()
 			else:
-				print("{0} no es una opcion valida".format(opcion))
+				print(Idioma.diccionarioMensajes.get("opcionNoValida").format(opcion))
                
 if __name__ == "__main__":
-   m=Main()
-   m.run()
+   Main().run()
 
 
 
